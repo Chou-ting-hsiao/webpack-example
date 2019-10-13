cd2da4373586e28668bd61501af2c24f59aa166f
Chou-ting-hsiao <kk156868@gmail.com>
2019-10-13 16:12:36 +0000

    change the image in assets

--- src/app/Home/app.component.ts
+++ src/app/Home/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+// import * as wasm from 'wasm-game-of-life';
+import { environment } from '../../environments/environment';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements  OnInit {
   title = 'webpack-example';
+
+  ngOnInit() {
+   
+    // console.log(wasm.get_number());
+    console.log(environment.api);
+
+  }
+  
 }
